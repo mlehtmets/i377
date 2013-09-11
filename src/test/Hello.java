4da3f4ecb5a5cdd09dda54494062a81c53ef6168
mlehtmet <Lehtmets4@gmail.com>
2013-09-11 10:24:39 +0300

    second commit

--- src/test/Hello.java
+++ src/test/Hello.java
@@ -13,7 +13,7 @@ public class Hello extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().println("Hello");
+		response.getWriter().println("Hello world");
 	}
 
 	/**
