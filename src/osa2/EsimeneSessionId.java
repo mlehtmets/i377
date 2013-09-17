618075582f6196c3f4f0dda993b07917db67d4e4
mlehtmet <Lehtmets4@gmail.com>
2013-09-17 13:30:25 +0300

    Projekt osa 2

---
+++ src/osa2/EsimeneSessionId.java
@@ -0,0 +1,29 @@
+package osa2;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class EsimeneSessionId extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter pw = response.getWriter();
+		// Tervitus
+		pw.println("Hello<br>");
+		
+		//Sessiooni id saamine
+		response.setContentType("text/html");
+		HttpSession session = request.getSession();
+		String ssnId = session.getId();
+		pw.println("Your session Id is " + ssnId + "<br>");
+		
+		//Sessiooni atribuudi seadmine
+		pw.println("Session attribute is " + request.getParameter("param") + "<br>");
+	}
+
+}
--- src/osa2/KolmasLogOut.java
+++ src/osa2/KolmasLogOut.java
@@ -1,27 +1,22 @@
-package test;
-
+package osa2;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.servlet.http.HttpSessionEvent;
-import javax.servlet.http.HttpSessionListener;
 
 /**
- * Servlet implementation class Hello
+ * Servlet implementation class KolmasLogOut
  */
-public class Hello extends HttpServlet {
+public class KolmasLogOut extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		PrintWriter pw = response.getWriter();
-		// Tervitus
-		pw.println("Hello");
-		
+		HttpSession session = request.getSession();
+		session.invalidate();
 	}
+
 }
